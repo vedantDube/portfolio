6e3be707ff53ca4f500db706a8161af9730089b3
vedhem999 <161755713+vedantDube@users.noreply.github.com>
2025-02-05 12:04:24 +0530

    add

---
+++ app/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+
+const Footer = ({ isDarkMode }) => {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image
+          src={isDarkMode ? assets.logo_dark : assets.logo}
+          alt=""
+          className="w-36 mx-auto mb-2"
+        />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />
+          vedhem999@gmail.com
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>Vedant Dubey</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/vedantDube" />
+            Github
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/vedant-dubey-55a934287/"
+            />
+            LinkedIn
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/Services.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import { assets, serviceData } from "@/assets/assets";
+import { motion } from "motion/react";
+
+const Services = ({ isDarkMode }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        What I offer
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        My Services
+      </motion.h2>
+
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.7, delay: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+      >
+        lorem ipsum ihsahsfiohiosfhiofashoiasfhoiasfhiosfhiofsahio
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.9, delay: 0.6 }}
+        className="grid grid-cols-auto gap-6 my-10"
+      >
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
+          >
+            <Image src={icon} alt="" className="w-10" />
+            <h3 className="text-lg my-4 text-gray-700 dark:text-white">
+              {" "}
+              {title}
+            </h3>
+            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
+              {description}
+            </p>
+            <a href={link}>
+              Read more
+              <Image src={assets.right_arrow} alt="" className="w-4" />
+            </a>
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
